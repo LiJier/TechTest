17565f9b492a691259d817094cad73272d632e41
李杰 <lijie@lijiedeMacBook-Pro.local>
2021-03-24 11:01:14 +0800

    init project

---
+++ app/src/main/java/com/lijie/techtest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.lijie.techtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
